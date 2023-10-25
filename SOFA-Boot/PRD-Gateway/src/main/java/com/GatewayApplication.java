baa6f3229f0b17f897e80624cd4fe426f531d2eb
xiaohei <1597226206@qq.com>
2023-10-25 22:08:03 +0800

    初始化网关模块

--- SOFA-Boot/PRD-Gateway/src/main/java/com/GatewayApplication.java
+++ SOFA-Boot/PRD-Gateway/src/main/java/com/GatewayApplication.java
@@ -2,10 +2,20 @@ package com;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
-@SpringBootApplication()
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class GatewayApplication {
     public static void main(String[] args) {
         SpringApplication.run(GatewayApplication.class, args);
     }
+    @Bean
+    @LoadBalanced
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+
 }
